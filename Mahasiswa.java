f690d52c953516a08ddc64b68bc166cef2e5ce41
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-04 12:07:18 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,13 @@
+public class Mahasiswa {
+    public void membaca() {
+        System.out.println("Mahasiswa sedang membaca koran.");
+    }
+
+    public void nyontek() {
+        System.out.println("Mahasiswa sedang menyontek saat ulangan.");
+    }
+
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi hasil tugas.");
+    }
+}
