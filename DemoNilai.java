9262c0c7cd958bd1cdb0a90894044a58fa7854c2
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-04 12:24:59 +0700

    Create DemoNilai.java

---
+++ DemoNilai.java
@@ -0,0 +1,6 @@
+public class DemoNilai {
+    public static void main(String[] args) {
+        Nilai mhs1 = new Nilai("2410631170155", "Dandy", 100, 90, 82, 80);
+        mhs1.tampilkanNilai();
+    }
+}
