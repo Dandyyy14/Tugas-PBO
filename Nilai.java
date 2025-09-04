bf04f5a1a6f1f532858f9a1ac85d4470481d2153
Dandyyy14 <muhammaddandy1899@gmail.com>
2025-09-04 12:24:16 +0700

    Create Nilai.java

---
+++ Nilai.java
@@ -0,0 +1,26 @@
+public class Nilai {
+    private String nim;
+    private String nama;
+    private double absen, tugas, uts, uas, akhir;
+
+    public Nilai(String nim, String nama, double absen, double tugas, double uts, double uas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.absen = absen;
+        this.tugas = tugas;
+        this.uts = uts;
+        this.uas = uas;
+
+        this.akhir = (absen * 0.1) + (tugas * 0.2) + (uts * 0.3) + (uas * 0.4);
+    }
+
+    public void tampilkanNilai() {
+        System.out.println("NIM              : " + nim);
+        System.out.println("Nama             : " + nama);
+        System.out.println("Nilai Absen[10%] : " + absen);
+        System.out.println("Nilai Tugas[20%] : " + tugas);
+        System.out.println("Nilai UTS [30%]  : " + uts);
+        System.out.println("Nilai UAS [40%]  : " + uas);
+        System.out.println("Nilai Akhir      : " + akhir);
+    }
+}
